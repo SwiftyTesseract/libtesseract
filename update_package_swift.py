bf096261da821c19a6c4ffdf4e32d9c9faff5f90
Steven Sherry <steven.sherry@affinityforapps.com>
2021-03-06 01:21:25 -0600

    Release/0.2.0 rc (#8)
    * Update update_package_swift.py to use textwrap module for trimming common whitespace so that multiline text does not need to start at column 0 in the script file.
    
    * Fix whitespace in deliver.yml
    
    * Update publish workflow to (hopefully) publish release when tag has been created
    
    * Fix method for setting RELEASE_VERSION environment variables for later use
    
    * Auto-updating Package.swift

--- update_package_swift.py
+++ update_package_swift.py
@@ -1,5 +1,6 @@
 import subprocess
 import argparse
+import textwrap
 
 def write_package_swift(version):
     libtesseract_zip = f"libtesseract-{version}.xcframework.zip"
@@ -8,29 +9,31 @@ def write_package_swift(version):
     checksum = checksum_result.stdout.decode("utf-8").strip("\n")
 
     with open("Package.swift", "w") as package_swift:
-        package_swift.write(f"""// swift-tools-version:5.3
+        package = f"""\
+        // swift-tools-version:5.3
 
-import PackageDescription
+        import PackageDescription
 
-let package = Package(
-  name: "libtesseract",
-  products: [
-    .library(
-      name: "libtesseract",
-      targets: ["libtesseract"]
-    ),
-  ],
-  dependencies: [],
-  targets: [
-    .binaryTarget(
-      name: "libtesseract",
-      url: "{download_url}",
-      checksum: "{checksum}"
-    )
-  ]
-)
+        let package = Package(
+          name: "libtesseract",
+          products: [
+            .library(
+              name: "libtesseract",
+              targets: ["libtesseract"]
+            ),
+          ],
+          dependencies: [],
+          targets: [
+            .binaryTarget(
+              name: "libtesseract",
+              url: "{download_url}",
+              checksum: "{checksum}"
+            )
+          ]
+        )
 
-""")
+        """
+        package_swift.write(textwrap.dedent(package))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Update libtesseract Package.swift")

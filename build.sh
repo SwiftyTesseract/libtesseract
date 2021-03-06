cb4a695e38c9e82937b06e4c743e2d6d591105fe
Steven Sherry <steven.sherry@affinityforapps.com>
2021-03-05 21:48:15 -0600

    Feature/m1 and friends (#6)
    * Initial run at m1 support
    
    * Fix task call

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-build() {
-  make platform=$1
-  cp -r $1/include/*.h include
-  cp -r $1/include/*.a .
-
-  libtool -static -o combined.a libjpeg.a liblept.a libpng.a libtiff.a libtesseract.a
-}
---
+++ update_package_swift.py
@@ -0,0 +1,39 @@
+import subprocess
+import argparse
+
+def write_package_swift(version):
+    libtesseract_zip = f"libtesseract-{version}.xcframework.zip"
+    download_url = f"https://github.com/SwiftyTesseract/libtesseract/releases/download/{version}/{libtesseract_zip}"
+    checksum_result = subprocess.run(["swift", "package", "compute-checksum", libtesseract_zip], stdout=subprocess.PIPE)
+    checksum = checksum_result.stdout.decode("utf-8").strip("\n")
+
+    with open("Package.swift", "w") as package_swift:
+        package_swift.write(f"""// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+  name: "libtesseract",
+  products: [
+    .library(
+      name: "libtesseract",
+      targets: ["libtesseract"]
+    ),
+  ],
+  dependencies: [],
+  targets: [
+    .binaryTarget(
+      name: "libtesseract",
+      url: "{download_url}",
+      checksum: "{checksum}"
+    )
+  ]
+)
+
+""")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Update libtesseract Package.swift")
+    parser.add_argument("--version", required=True, help="The version to update Package.swift for")
+    args = parser.parse_args()
+    write_package_swift(args.version)
